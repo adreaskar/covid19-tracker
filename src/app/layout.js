bebfee121f7fe7e181410c7ca63d60e389bbbf12
adreaskar <adrian26mccous@gmail.com>
2024-04-11 13:10:20 +0300

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -12,6 +13,21 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <Script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                        `,
+                    }}
+                />
                 <link rel="icon" href="/images/favicon.png" />
             </head>
             <body className={inter.className}>
